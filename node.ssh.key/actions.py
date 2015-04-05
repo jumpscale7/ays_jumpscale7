d4ea74db05029a52ae8a8a2ed7d20502246188c6
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-04-05 10:50:13 +0400

    add node.ssh.key, sshkey services

---
+++ node.ssh.key/actions.py
@@ -0,0 +1,79 @@
+from JumpScale import j
+from JumpScale.baselib.atyourservice.ActionsBase import remote
+ActionsBase=j.atyourservice.getActionsBaseClass()
+
+
+import JumpScale.baselib.remote.cuisine
+
+class Actions(ActionsBase):
+
+    def configure(self,serviceObj):
+        """
+        will install a node over ssh
+        """
+        def update():
+            j.do.execute("apt-get update")
+        j.actions.start(name="update",description='update', action=update, stdOutput=True, serviceObj=serviceObj)
+
+        def upgrade():
+            j.do.execute("apt-get upgrade -y")
+        j.actions.start( name="upgrade",description='upgrade', action=upgrade, stdOutput=True, serviceObj=serviceObj)
+        def extra():
+            j.do.execute("apt-get install byobu curl -y")
+        j.actions.start(name="extra",description='extra', action=extra, stdOutput=True, serviceObj=serviceObj)
+
+        def jumpscale():
+            j.do.execute("curl https://raw.githubusercontent.com/Jumpscale/jumpscale_core7/master/install/install_python_web.sh > /tmp/installjs.sh")
+            j.do.execute("sh /tmp/installjs.sh")
+        j.actions.start(name="jumpscale",description='install jumpscale', action=jumpscale, stdOutput=True, serviceObj=serviceObj)
+
+        return True
+
+
+    def removedata(self,serviceObj):
+        """
+        delete vmachine
+        """
+        j.do.execute("killall tmux;killall python;echo")
+        # j.do.execute("rm -rf /opt")
+        return True
+
+    def execute(self,serviceObj,cmd):
+        # j.do.execute(cmd)
+        # ip = serviceObj.hrd.get("instance.machine.ssh.ip")
+        # port = serviceObj.hrd.get("instance.machine.ssh.port")
+        # keyname =  serviceObj.hrd.get('instance.ssh.key.name') if serviceObj.hrd.exists('instance.ssh.key.name') else None
+        # login = serviceObj.hrd.get('instance.ssh.user') if serviceObj.hrd.exists('instance.ssh.user') else None
+        # password = serviceObj.hrd.get('instance.ssh.pwd') if serviceObj.hrd.exists('instance.ssh.pwd') else None
+        # keyHRD = j.application.getAppInstanceHRD("sshkey",keyname) if keyname != None else None
+        sshkey = self._getSSHKey(serviceObj)
+        ip = serviceObj.hrd.get("instance.ip")
+        port = serviceObj.hrd.get("instance.ssh.port")
+
+        c = j.remote.cuisine
+        c.fabric.env["key"] = sshkey
+        cl = c.connect(ip,port)
+        cl.run(cmd)
+
+
+    def upload(self, serviceObj,source,dest):
+        sshkey = self._getSSHKey(serviceObj)
+
+        ip = serviceObj.hrd.get("instance.ip")
+        port = serviceObj.hrd.get("instance.ssh.port")
+        dest = "%s:%s" % (ip,dest)
+        self._rsync(source,dest,sshkey,port)
+
+    def download(self, serviceObj,source,dest):
+        sshkey = self._getSSHKey(serviceObj)
+
+        ip = serviceObj.hrd.get("instance.ip")
+        port = serviceObj.hrd.get("instance.ssh.port")
+        source = "%s:%s" % (ip,source)
+        self._rsync(source,dest,sshkey,port)
+
+    def _getSSHKey(self,serviceObj):
+        keyname = serviceObj.hrd.get("link.sshkey.instance")
+        sshkeyHRD = j.application.getAppInstanceHRD("sshkey",keyname)
+        sshkey = sshkeyHRD.get("instance.key.priv")
+        return sshkey
---
+++ sshkey/actions.py
@@ -0,0 +1,49 @@
+from JumpScale import j
+
+ActionsBase = j.atyourservice.getActionsBaseClass()
+
+import JumpScale.baselib.remote.cuisine
+
+class Actions(ActionsBase):
+
+    def prepare(self, serviceObj):
+        if serviceObj.hrd.get("instance.key.priv").strip() == "":
+            self._generateKeys(serviceObj)
+        elif serviceObj.hrd.get("instance.key.pub"):
+            privkey = serviceObj.hrd.get("instance.key.priv")
+            j.do.writeFile("/tmp/privkey", privkey)
+            j.do.chmod('/tmp/privkey', 0o600)
+            cmd = 'ssh-keygen -f /tmp/privkey -y -N \'\'> \'/tmp/pubkey\''
+            j.do.execute(cmd)
+            j.do.chmod('/tmp/pubkey', 0o600)
+            pubkey = j.do.readFile('/tmp/pubkey')
+            serviceObj.hrd.set("instance.key.pub", pubkey)
+
+
+    def _generateKeys(self,serviceObj):
+        keyloc = "/tmp/id_dsa"
+        j.system.process.executeWithoutPipe("ssh-keygen -t dsa -f %s" % keyloc)
+        if not j.system.fs.exists(path=keyloc):
+            raise RuntimeError("cannot find path for key %s, was keygen well executed" % keyloc)
+
+        key = j.system.fs.fileGetContents(keyloc)
+        keypub = j.system.fs.fileGetContents(keyloc + ".pub")
+
+        serviceObj.hrd.set("instance.key.pub", keypub)
+        serviceObj.hrd.set("instance.key.priv", key)
+
+    def configure(self, serviceObj):
+        """
+        create key
+        """
+
+        if serviceObj.hrd.get("instance.key.priv").strip() == "":
+            self._generateKeys(serviceObj)
+
+        return True
+
+    def removedata(self, serviceObj):
+        """
+        remove key data
+        """
+        pass
