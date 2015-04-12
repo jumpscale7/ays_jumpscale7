7dd3d977d3011b509f05e7be42c2a72b8d328385
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-04-12 08:54:11 +0400

    rollback mongodb_client instance.hrd

---
+++ heka-master/actions.py
@@ -0,0 +1,35 @@
+from JumpScale import j
+
+ActionsBase=j.atyourservice.getActionsBaseClass()
+
+import os.path
+
+def log(message):
+    print(' :: ' + message)
+
+
+class Actions(ActionsBase):
+
+
+    def build(self, serviceObj):
+    
+        BIN_TARBALL_URL = 'https://github.com/mozilla-services/heka/releases/download/v0.8.3/heka-0_8_3-linux-amd64.tar.gz'
+        BUILD_BASE = '/opt/build/heka-master/'
+        BIN_TARBALL_PATH = os.path.join(BUILD_BASE, 'heka-0_8_3-linux-amd64.tar.gz')
+        BIN_ROOT = os.path.join(BUILD_BASE, 'heka-0_8_3-linux-amd64')   # The tarball will be extracted into this
+
+        j.system.fs.createDir(BUILD_BASE)
+
+        log('Downloading binary tarball')
+        j.system.net.downloadIfNonExistent(BIN_TARBALL_URL, BIN_TARBALL_PATH, md5_checksum='d23d0e599263b5111bf310fbea838ff3')
+
+        log('Uncompressing binary tarball')
+        j.system.fs.targzUncompress(BIN_TARBALL_PATH, BUILD_BASE, removeDestinationdir=False)
+        
+
+    def configure(self, serviceObj):
+
+        INSTALLATION_CONFIGS_PATH = os.path.join(serviceobject.hrd.get('service.base'), 'master-configs') 
+
+        influxdb_client_hrd = j.application.getAppInstanceHRD('influxdb_client', 'main')
+        influxdb_client_hrd.applyOnDir(INSTALLATION_CONFIGS_PATH)
