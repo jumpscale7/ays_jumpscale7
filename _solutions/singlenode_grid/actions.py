697c27a4178dc15106b49a3002684d539b9b8ab0
Christophe de Carvalho <christophe@incubaid.com>
2015-05-12 10:45:00 +0400

    set role master during singleonde_grid installation

--- _solutions/singlenode_grid/actions.py
+++ _solutions/singlenode_grid/actions.py
@@ -3,4 +3,12 @@ from JumpScale import j
 ActionsBase=j.atyourservice.getActionsBaseClass()
 
 class Actions(ActionsBase):
-    pass
+
+    def prepare(self, serviceObj):
+        roles = j.application.config.getList('grid.node.roles')
+        if 'master' not in roles:
+            roles.append('master')
+            j.application.config.set('grid.node.roles', roles)
+
+        # reload system config / whoAmI
+        j.application.loadConfig()
