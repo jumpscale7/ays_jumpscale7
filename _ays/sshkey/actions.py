a48a177bc27118970fd7dd79e985b1fd1a160c30
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-05-21 09:00:47 +0400

    replace heka-master with statsd-master for singlenodegrid

--- _ays/sshkey/actions.py
+++ _ays/sshkey/actions.py
@@ -10,19 +10,23 @@ class Actions(ActionsBase):
         if serviceObj.hrd.get("instance.key.priv").strip() == "":
             self._generateKeys(serviceObj)
         elif serviceObj.hrd.get("instance.key.pub"):
+            privloc = "/tmp/privkey"
+            publoc = '/tmp/pubkey'
             privkey = serviceObj.hrd.get("instance.key.priv")
-            j.do.writeFile("/tmp/privkey", privkey)
-            j.do.chmod('/tmp/privkey', 0o600)
-            cmd = 'ssh-keygen -f /tmp/privkey -y -N \'\'> \'/tmp/pubkey\''
+            j.do.writeFile(privloc, privkey)
+            j.do.chmod(privloc, 0o600)
+            cmd = 'ssh-keygen -f %s -y -N \'\'> \'%s\'' % (privloc,publoc)
             j.do.execute(cmd)
-            j.do.chmod('/tmp/pubkey', 0o600)
-            pubkey = j.do.readFile('/tmp/pubkey')
+            j.do.chmod(publoc, 0o600)
+            pubkey = j.do.readFile(publoc)
             serviceObj.hrd.set("instance.key.pub", pubkey)
+            j.system.fs.remove(publoc)
+            j.system.fs.remove(privloc)
 
 
     def _generateKeys(self,serviceObj):
         keyloc = "/tmp/id_dsa"
-        j.system.process.executeWithoutPipe("ssh-keygen -t dsa -f %s" % keyloc)
+        j.system.process.executeWithoutPipe("ssh-keygen -t dsa -f %s -P '' " % keyloc)
         if not j.system.fs.exists(path=keyloc):
             raise RuntimeError("cannot find path for key %s, was keygen well executed" % keyloc)
 
@@ -31,6 +35,9 @@ class Actions(ActionsBase):
 
         serviceObj.hrd.set("instance.key.pub", keypub)
         serviceObj.hrd.set("instance.key.priv", key)
+
+        j.system.fs.remove(keyloc)
+        j.system.fs.remove(keyloc+".pub")
 
     def configure(self, serviceObj):
         """
