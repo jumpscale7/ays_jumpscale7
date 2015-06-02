286c0b415408c7fe59d464264089415126d6606f
Muhamad Azmy <muhamad.azmy@codescalers.com>
2015-06-02 10:50:08 +0200

    Implement package build for hubble

--- _solutions/hubble/actions.py
+++ _solutions/hubble/actions.py
@@ -1,6 +1,34 @@
+import os
+
 from JumpScale import j
-# from JumpScale.baselib.atyourservice.ActionsBase import remote
-ActionsBase=j.atyourservice.getActionsBaseClass()
+
+ActionsBase = j.atyourservice.getActionsBaseClass()
+
 
 class Actions(ActionsBase):
-    pass
+    def build(self, service_obj):
+        bashrc = os.environ['HOME'] + "/" + ".bashrc"
+
+        root = '/opt/build/git.aydo.com/0-complexity/hubble'
+
+        src = '{root}/src'.format(root=root)
+
+        if not os.path.exists(src):
+            j.do.execute(
+                'mkdir %s' % src
+            )
+
+        cmd = (
+            '. {bashrc} && GOPATH=$GOPATH:{root}/ && cp -r {root}/hubble/ ' +
+            '{root}/src && cd {root}/src/hubble/ && godep get && cd ./main' +
+            ' && go build proxy.go && go build agent.go && ' +
+            'cp {root}/src/hubble/main/proxy {root}/src/hubble/main/agent ' +
+            '/opt/code/git/binary/hubble'
+        ).format(bashrc=bashrc, root=root)
+
+        j.do.execute(cmd)
+        j.do.pushGitRepos(
+            message='hubble new build',
+            name='hubble',
+            account='binary'
+        )
