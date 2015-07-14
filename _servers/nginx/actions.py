b9d6da2a4a42248917ec92e8a61cd1e9716ae580
Rob Van Mieghem <rob@vanmieghemcloud.com>
2015-07-14 15:35:08 +0200

    No need to brutely destroy existing nginx's

--- _servers/nginx/actions.py
+++ _servers/nginx/actions.py
@@ -3,25 +3,9 @@ import time
 ActionsBase=j.atyourservice.getActionsBaseClass()
 
 class Actions(ActionsBase):
-    
+
     def prepare(self, serviceObj):
         j.do.execute('apt-get install -y libgd3')
-        try:
-            j.system.platform.ubuntu.stopService("nginx")
-        except:
-            pass
-
-        cmd="killall nginx"
-        try:
-            j.system.process.execute(cmd)
-        except:
-            pass
-
-        j.system.platform.ubuntu.remove("nginx")
-        j.system.platform.ubuntu.remove("lua-nginx-memcached")
-        j.system.platform.ubuntu.remove("lua-nginx-redis")
-        j.system.platform.ubuntu.remove("nginx-common")
-        j.system.platform.ubuntu.remove("nginx-extras")
 
         #hack for sandboxed nginx to start properly
         j.system.fs.createDir(j.system.fs.joinPaths('/var', 'lib', 'nginx'))
@@ -33,8 +17,4 @@ class Actions(ActionsBase):
     def stop(self, serviceObj):
         if not j.system.fs.isDir('/opt/nginx'):
             return
-        if not j.system.process.getPidsByPort(80):
-            return
         j.system.process.execute('cd /opt/nginx && ./nginx -c /opt/nginx/cfg/nginx.conf -s quit')
-
-     
