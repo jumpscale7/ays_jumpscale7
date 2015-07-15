2742214cc07605dbffc6c9c86c3b9898b64a53fc
Rob Van Mieghem <rob@vanmieghemcloud.com>
2015-07-15 11:08:53 +0200

    Remove the custom stop for nginx completely

--- _servers/nginx/actions.py
+++ _servers/nginx/actions.py
@@ -13,8 +13,3 @@ class Actions(ActionsBase):
         logPath = j.system.fs.joinPaths('/var', 'log', 'nginx')
         if not j.system.fs.exists(logPath):
             j.system.fs.createDir(logPath)
-
-    def stop(self, serviceObj):
-        if not j.system.fs.isDir('/opt/nginx'):
-            return
-        j.system.process.execute('cd /opt/nginx && ./nginx -c /opt/nginx/cfg/nginx.conf -s quit')
