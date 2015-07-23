4ec281d821e283cd5cb9c3c608220432b0b538ad
kristof de spiegeleer <kristof@incubaid.com>
2015-07-23 14:58:42 +0200

    new

--- _servers/influxdb/actions.py
+++ _servers/influxdb/actions.py
@@ -4,8 +4,34 @@ ActionsBase = j.atyourservice.getActionsBaseClass()
 
 
 class Actions(ActionsBase):
+
+    def prepare(self,serviceObj):
+        """
+        this gets executed before the files are downloaded & installed on approprate spots
+        """
+
+        if j.do.TYPE.lower().startswith("osx"):
+            res=j.do.execute("brew install influxdb")
+            res=j.do.execute("brew list influxdb")
+            for line in res[1].split("\n"):
+                if line.strip()=="":
+                    continue
+                if j.do.exists(line.strip()) and line.find("bin/")!=-1:
+                    destpart=line.split("bin/")[-1]
+                    dest="/opt/influxdb/%s"%destpart
+                    j.system.fs.createDir(j.system.fs.getDirName(dest))
+                    j.do.copyFile(line,dest)
+                    j.do.chmod(dest, 0o770) 
+            cmd='mkdir -p /opt/influxdb/cfg;curl -k https://git.aydo.com/binary/influxdb/raw/master/cfg/influxdb/influxdb.conf > /opt/influxdb/cfg/influxdb.conf'
+            j.do.execute(cmd)
+        return True
+
     def configure(self, service):
-        fname = '$(service.param.base)/cfg/influxdb/config.toml'
+        if j.do.TYPE.lower().startswith("osx"):
+            fname = '$(service.param.base)/cfg/influxdb.conf'
+        else:
+            fname = '$(service.param.base)/cfg/config.toml'
+
         cfg = j.system.fs.fileGetContents(fname)
 
         cfg = j.dirs.replaceTxtDirVars(cfg, additionalArgs={})
