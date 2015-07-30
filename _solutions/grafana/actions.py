ef15c95d63002f627316bcdc43d6551a2dc07e02
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-07-30 15:48:51 +0200

    update grafana to 2.1

--- _solutions/grafana/actions.py
+++ _solutions/grafana/actions.py
@@ -1,13 +1,16 @@
 from JumpScale import j
-import time
-import socket
-import fcntl
-import struct
+import requests
+import json
 
 ActionsBase = j.atyourservice.getActionsBaseClass()
 
+
 class Actions(ActionsBase):
-    
+
+    def prepare(self, serviceObj):
+        j.system.fs.createDir("/opt/grafana")
+        j.system.fs.createDir("/opt/grafana/conf")
+        j.system.fs.createDir("/opt/grafana/public")
 
     def configure(self, serviceObj):
         """
@@ -17,17 +20,47 @@ class Actions(ActionsBase):
         """
         influx_instance = serviceObj.hrd.get('instance.param.influxdb.connection')
         hrd = j.application.getAppInstanceHRD('influxdb_client', influx_instance)
-        host = hrd.get('instance.param.influxdb.client.address')
+        host = hrd.getStr('instance.param.influxdb.client.address')
+        port = hrd.getInt('instance.param.influxdb.client.port')
+        login = hrd.getStr('instance.param.influxdb.client.login')
+        passwd = hrd.getStr('instance.param.influxdb.client.passwd')
+        dbname = hrd.getStr('instance.param.influxdb.client.dbname')
 
-        if j.system.net.isIpLocal(host):
-            host = 'window.location.hostname'
-        else:
-            host = "'%s'" % host
+        data = {
+          'type': 'influxdb',
+          'access': 'proxy',
+          'database': dbname,
+          'name': 'influxdb_main',
+          'url': 'http://%s:%u' % (host, port),
+          'user': login,
+          'password': passwd,
+          'default': True,
+        }
 
-        configsamplepath = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'portals', 'jslib', 'grafana', 'config.sample.js')
-        configpath = j.system.fs.joinPaths(j.dirs.baseDir, 'apps', 'portals', 'jslib', 'grafana', 'config.js')
-        if not j.system.fs.exists(configpath):
-            j.system.fs.createEmptyFile(configpath)
+        # need to start the grafana backend server to enable http api
+        serviceObj.start()
 
-        j.system.fs.copyFile(configsamplepath, configpath)
-        hrd.applyOnFile(configpath)
+        # check if the datasource already exists
+        grafanaAPI = 'http://admin:admin@localhost:%s/api/datasources' % serviceObj.hrd.get('instance.param.port')
+
+        try:
+            resp = requests.get(grafanaAPI)
+        except Exception as e:
+            from ipdb import set_trace;set_trace()
+            j.events.opserror_critical(e.message)
+
+        datasources = json.loads(resp.content)
+        present = False
+        for ds in datasources:
+            if ds['url'] == data['url'] and ds['user'] == data['user'] and \
+               ds['password'] == data['password'] and ds['access'] == data['access']:
+               present = True
+
+        if not present:
+
+            # create the datasource for influxdb
+            try:
+                resp = requests.post(grafanaAPI, json=data,
+                                     headers={'content-type': 'application/json'})
+            except Exception as e:
+                j.events.opserror_critical(e.message)
