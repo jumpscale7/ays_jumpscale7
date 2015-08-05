79ff3fa47fe3b5bf390066439800144bf8e17656
Maxime Daniel <root@maxux.net>
2015-08-05 16:29:32 +0200

    fix samba 4 indentation and improve jumpscale usage

--- _servers/samba4/actions.py
+++ _servers/samba4/actions.py
@@ -22,8 +22,8 @@ class Actions(ActionsBase):
         """
         this gets executed before the files are downloaded & installed on appropriate spots
         """
-	# Download keyring for sernet repository
-	j.system.net.download('http://ftp.sernet.de/pub/sernet-samba-keyring_1.4_all.deb', '/tmp/')
+        # Download keyring for sernet repository
+        j.system.net.download('http://ftp.sernet.de/pub/sernet-samba-keyring_1.4_all.deb', '/tmp/')
         return True
 
     def configure(self,serviceObj):
@@ -32,45 +32,44 @@ class Actions(ActionsBase):
         this step is used to do configuration steps to the platform
         after this step the system will try to start the jpackage if anything needs to be started
         """
-	# Install sernet repository on system
+        # Install sernet repository on system
         j.system.process.run("dpkg -i /tmp/sernet-samba-keyring_1.4_all.deb", True, False)
 
-	# Check if sources.list contains sernet packages server
-	if 'download.sernet.de/packages/samba/4.2/ubuntu' not in open('/etc/apt/sources.list').read():
-		print 'Adding sources.list sernet repository'
-		with open('/etc/apt/sources.list', 'a') as sourceslist:
-			sourceslist.write("\n")
-			sourceslist.write("# Samba sernet\n")
-			sourceslist.write("deb https://sernet-samba-public:Noo1oxe4zo@download.sernet.de/packages/samba/4.2/ubuntu trusty main\n")
+        # Check if sources.list contains sernet packages server
+        if 'download.sernet.de/packages/samba/4.2/ubuntu' not in open('/etc/apt/sources.list').read():
+            print 'Adding sources.list sernet repository'
+            with open('/etc/apt/sources.list', 'a') as sourceslist:
+                  sourceslist.write("\n")
+                  sourceslist.write("# Samba sernet\n")
+                  sourceslist.write("deb https://sernet-samba-public:Noo1oxe4zo@download.sernet.de/packages/samba/4.2/ubuntu trusty main\n")
 
-	# Update packages list
-	j.system.process.run("apt-get update", True, False)
+        # Update packages list
+        j.system.platform.ubuntu.updatePackageMetadata()
 
-	# Install samba
-	j.system.process.run("apt-get install -y sernet-samba-client sernet-samba-ad", True, False)
-	j.system.fs.createDir('/var/run/samba/')
+        # Install samba
+        j.system.platform.ubuntu.install("sernet-samba-client sernet-samba-ad")
+        j.system.fs.createDir('/var/run/samba/')
 
-	# Loading hrd settings
-	domain = serviceObj.hrd.get('instance.param.ad.domain')
-	realm  = serviceObj.hrd.get('instance.param.ad.realm')
-	passwd = serviceObj.hrd.get('instance.param.ad.adminpwd')
-        
-	# Skipping AD if no domain is set
-	if not domain == '*':
-		print 'Setting up Active Directory:', realm, '/', domain
-		# Removing old files
-		j.system.fs.moveFile('/etc/samba/smb.conf', '/etc/samba/smb.conf.bak')
+        # Loading hrd settings
+        domain = serviceObj.hrd.get('instance.param.ad.domain')
+        realm  = serviceObj.hrd.get('instance.param.ad.realm')
+        passwd = serviceObj.hrd.get('instance.param.ad.adminpwd')
 
-		# Provision AD
-		options = "--domain " + domain + " --realm " + realm + " --adminpass " + passwd
-		j.system.process.run("samba-tool domain provision --use-rfc2307 " + options, True, False)
-		
-		# Update resolv.conf
-		with open('/etc/resolv.conf', 'w') as resolv:
-			resolv.write("domain " + realm + "\n")
-			resolv.write("nameserver 127.0.0.1\n")
-			
-	else:
-		print 'Active Directory not enabled, skipping'
-	
-	return True
+        # Skipping AD if no domain is set
+        if not domain == '*':
+            print 'Setting up Active Directory:', realm, '/', domain
+            # Removing old files
+            j.system.fs.moveFile('/etc/samba/smb.conf', '/etc/samba/smb.conf.bak')
+
+            # Provision AD
+            options = "--domain " + domain + " --realm " + realm + " --adminpass " + passwd
+            j.system.process.run("samba-tool domain provision --use-rfc2307 " + options, True, False)
+
+            # Update resolv.conf
+            j.system.fs.writeFile('/etc/resolv.conf', "domain " + realm + "\n", False)
+            j.system.fs.writeFile('/etc/resolv.conf', "nameserver 127.0.0.1\n", True)
+              
+        else:
+            print 'Active Directory not enabled, skipping'
+
+        return True
