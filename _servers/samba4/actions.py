7ceeacc057b9b9e4bacbc7f98d86235cf8d0d705
Maxime Daniel <root@maxux.net>
2015-08-07 17:12:00 +0200

    staging changes on samba4 with first member join support

--- _servers/samba4/actions.py
+++ _servers/samba4/actions.py
@@ -1,4 +1,5 @@
 from JumpScale import j
+import signal
 
 ActionsBase=j.atyourservice.getActionsBaseClass()
 
@@ -22,8 +23,6 @@ class Actions(ActionsBase):
         """
         this gets executed before the files are downloaded & installed on appropriate spots
         """
-        # Download keyring for sernet repository
-        j.system.net.download('http://ftp.sernet.de/pub/sernet-samba-keyring_1.4_all.deb', '/tmp/')
         return True
 
     def configure(self,serviceObj):
@@ -32,8 +31,11 @@ class Actions(ActionsBase):
         this step is used to do configuration steps to the platform
         after this step the system will try to start the jpackage if anything needs to be started
         """
+        base = j.application.config.get('system.paths.base')
+        base = base + '/apps/samba4'
+        
         # Install sernet repository on system
-        j.system.process.run("dpkg -i /tmp/sernet-samba-keyring_1.4_all.deb", True, False)
+        j.system.process.run("dpkg -i " + base + "/sernet-samba-keyring_1.4_all.deb", True, False)
 
         # Check if sources.list contains sernet packages server
         if 'download.sernet.de/packages/samba/4.2/ubuntu' not in open('/etc/apt/sources.list').read():
@@ -42,25 +44,60 @@ class Actions(ActionsBase):
             j.system.fs.writeFile('/etc/apt/sources.list', "# Samba sernet\n", True)
             j.system.fs.writeFile('/etc/apt/sources.list', "deb https://sernet-samba-public:Noo1oxe4zo@download.sernet.de/packages/samba/4.2/ubuntu trusty main\n", True)
 
-        # Update packages list
-        j.system.platform.ubuntu.updatePackageMetadata()
+            # Update packages list
+            j.system.platform.ubuntu.updatePackageMetadata()
 
         # Install samba
         j.system.platform.ubuntu.install("sernet-samba-client sernet-samba-ad")
         j.system.fs.createDir('/var/run/samba/')
 
         # Loading hrd settings
+        myaddr = serviceObj.hrd.get('instance.param.ad.ipaddr')
         domain = serviceObj.hrd.get('instance.param.ad.domain')
         realm  = serviceObj.hrd.get('instance.param.ad.realm')
+        remote = serviceObj.hrd.get('instance.param.ad.remote')
         passwd = serviceObj.hrd.get('instance.param.ad.adminpwd')
+        
+        # Saving old files
+        if j.system.fs.exists('/etc/samba/smb.conf'):
+            j.system.fs.moveFile('/etc/samba/smb.conf', '/etc/samba/smb.conf.bak')
 
         # Skipping AD if no domain is set
-        if not domain == '*':
-            print 'Setting up Active Directory:', realm, '/', domain
+        if not remote == '':
+            print 'Joining Active Directory', realm, '/', myaddr
             
-            # Removing old files
-            if j.system.fs.exists('/etc/samba/smb.conf'):
-                j.system.fs.moveFile('/etc/samba/smb.conf', '/etc/samba/smb.conf.bak')
+            # Update resolv.conf
+            j.system.fs.writeFile('/etc/resolv.conf', "domain " + realm + "\n", False)
+            j.system.fs.writeFile('/etc/resolv.conf', "nameserver " + remote + "\n", True)
+            
+            # Setup /etc/hosts
+            
+            
+            # Building smb.conf file
+            j.system.fs.copyFile(base + '/smb.member.conf', '/etc/samba/smb.conf')
+            
+            data = {
+                'hostname': j.system.net.getHostname(),
+                'workgroup': domain,
+                'realm': realm
+            }
+            
+            hrd = j.core.hrd.getHRDFromDict(data)
+            hrd.applyOnFile('/etc/samba/smb.conf')
+            
+            # Reload config
+            j.system.process.killProcessByName('smbd', signal.SIGHUP)
+            j.system.process.killProcessByName('nmbd', signal.SIGHUP)
+            
+            # Joining domain
+            j.system.process.run("net ads join -U Administrator%" + passwd, True, False, 10, False)
+            
+            # Enable remote auth with nsswitch.conf
+            
+            
+            
+        else:
+            print 'Setting up new Active Directory:', realm, '/', domain
 
             # Provision AD
             options = "--domain " + domain + " --realm " + realm + " --adminpass " + passwd
@@ -69,8 +106,5 @@ class Actions(ActionsBase):
             # Update resolv.conf
             j.system.fs.writeFile('/etc/resolv.conf', "domain " + realm + "\n", False)
             j.system.fs.writeFile('/etc/resolv.conf', "nameserver 127.0.0.1\n", True)
-              
-        else:
-            print 'Active Directory not enabled, skipping'
 
         return True
