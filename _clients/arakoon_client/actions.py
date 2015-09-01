5617671abc3cfe5ec212071eea714ae195c14907
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2015-09-01 11:50:43 +0200

    add arakoon_client

---
+++ _clients/arakoon_client/actions.py
@@ -0,0 +1,21 @@
+from JumpScale import j
+
+ActionsBase = j.atyourservice.getActionsBaseClass()
+
+
+class Actions(ActionsBase):
+
+    def configure(self, serviceObj):
+        def askNodeInfo(node):
+            nodeinfo = {}
+            nodeinfo['name'] = j.console.askString("Enter node name", node)
+            nodeinfo['ip'] = j.console.askString("Enter node IP")
+            nodeinfo['client_port'] = j.console.askInteger("Enter node client port", 7080)
+            nodeinfo['messaging_port'] = j.console.askInteger("Enter messaging port", 10000)
+            nodeinfo['home'] = j.console.askString("Enter node home dir", "/opt/arakoon/data/")
+            return nodeinfo
+
+        nodes = serviceObj.hrd.getList('instance.cluster')
+        for node in nodes:
+            info = askNodeInfo(node)
+            serviceObj.hrd.set('instance.%s' % node, info)
