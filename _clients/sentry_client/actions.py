01cd869112ffb9fa461ee8e052c1d2a7ac74fd46
Maxime Daniel <root@maxux.net>
2015-11-17 12:28:37 +0100

    fix dependancies for sentry_client

--- _clients/sentry_client/actions.py
+++ _clients/sentry_client/actions.py
@@ -6,4 +6,8 @@ class Actions(ActionsBase):
     """
     """
     def configure(self, serviceObj):
+        # do not use framework to install python-pip
+        # PYTHONPATH environment need to be cleared otherwise setup
+        # will fail
+        j.do.execute('PYTHONPATH="" apt-get install python-pip')
         j.do.execute('pip install raven')
